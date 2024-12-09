1ce9ecbdb57fb42b1b6888ebee22a76e24f18771
Zakariae zourkane <zourte2486@gmail.com>
2024-12-09 11:58:21 -0800

    12-09

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Counter from "./App";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <div>
-    <Counter />
+    </>
   </div>
 );
